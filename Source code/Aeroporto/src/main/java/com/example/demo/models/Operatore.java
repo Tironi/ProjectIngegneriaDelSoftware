53c5871c161268e3fa164d7d822f15d4c97a200e
a.belotti29 <a.belotti29@studenti.unibg.it>
2022-01-06 22:21:52 +0100

    Conversioni models Rebel->spring

--- Source code/Aeroporto/src/main/java/com/example/demo/models/Operatore.java
+++ Source code/Aeroporto/src/main/java/com/example/demo/models/Operatore.java
@@ -3,39 +3,18 @@
 *
 * Drop us a line or two at feedback@archetypesoftware.com: we would love to hear from you!
 */
-package Model1;
+package com.example.demo.models;
 
 import java.util.*;
-import java.time.*;
 
-
-
-// ----------- << imports@AAAAAAF9/M0bFeIb0tI= >>
-// ----------- >>
-
-// ----------- << class.annotations@AAAAAAF9/M0bFeIb0tI= >>
-// ----------- >>
 public class Operatore {
-    // ----------- << attribute.annotations@AAAAAAF9/M1AduLodPM= >>
-    // ----------- >>
+
     private String nome;
-
-    // ----------- << attribute.annotations@AAAAAAF9/NAwHeb1LB8= >>
-    // ----------- >>
     private String cognome;
-
-    // ----------- << attribute.annotations@AAAAAAF+JydpHXqGq5w= >>
-    // ----------- >>
     private String email;
-
-    // ----------- << attribute.annotations@AAAAAAF+Jye61XwVbTQ= >>
-    // ----------- >>
     private String PSW;
 
-    // ----------- << attribute.annotations@AAAAAAF9/OCsePMHhkw= >>
-    // ----------- >>
-    private Set<Volo> inserisceVolo = new HashSet<>();
-
+    
     private String getNome() {
         return nome;
     }
@@ -50,10 +29,6 @@ public class Operatore {
 
     private String getPSW() {
         return PSW;
-    }
-
-    public Set<Volo> getInserisceVolo() {
-        return inserisceVolo;
     }
 
     private void setNome(String nome) {
@@ -72,41 +47,15 @@ public class Operatore {
         this.PSW = PSW;
     }
 
-    public void linkInserisceVolo(Volo _inserisceVolo) {
-        if (_inserisceVolo != null) {
-            _inserisceVolo.unlinkInseritoDa();
-            _inserisceVolo.setInseritoDa(this);
-            getInserisceVolo().add(_inserisceVolo);
-        }
-    }
-
-    public void unlinkInserisceVolo(Volo _inserisceVolo) {
-        if (_inserisceVolo != null) {
-            _inserisceVolo.setInseritoDa(null);
-            getInserisceVolo().remove(_inserisceVolo);
-        }
-    }
-
-    public void unlinkInserisceVolo(Volo _inserisceVolo, Iterator<Volo> it) {
-        if (_inserisceVolo != null) {
-            _inserisceVolo.setInseritoDa(null);
-            it.remove();
-        }
-    }
-
     /**
+     * L'operatore nel momento in cui decide di inserire un nuovo volo
     * @param codiceAereo 
     * @param aeroporto 
     * @param orario 
     * @param prezzo
     */
 
-    // ----------- << method.annotations@AAAAAAF+JwWtNiP8vMM= >>
-    // ----------- >>
     public void inserisciVolo(String codiceAereo, String aeroporto, LocalDateTime orario, double prezzo) {
-    // ----------- << method.body@AAAAAAF+JwWtNiP8vMM= >>
-    // ----------- >>
+    
     }
-// ----------- << class.extras@AAAAAAF9/M0bFeIb0tI= >>
-// ----------- >>
 }
