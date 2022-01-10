7a340d4c6bfc62393bec9d8be81301d59205f656
a.belotti29 <a.belotti29@studenti.unibg.it>
2022-01-10 23:10:21 +0100

    update inserisci volo

--- Source code/Aeroporto/src/main/java/com/pack/aeroporto/model/Volo.java
+++ Source code/Aeroporto/src/main/java/com/pack/aeroporto/model/Volo.java
@@ -1,10 +1,21 @@
 package com.pack.aeroporto.model;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlElement;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 
 import java.time.*;
+import java.util.Date;
 
 @Entity
 public class Volo {
@@ -12,8 +23,13 @@ public class Volo {
     private @Id @GeneratedValue Long codice;
     private String aeroportoPartenza;
     private String aeroportoArrivo;
-    private LocalDateTime orario;
-    private int codiceAereo;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Basic(fetch = FetchType.EAGER)
+    @XmlElement
+    @DateTimeFormat(pattern = "dd-MM-yyyy'T'HH:mm")
+    private Date orario;
+    private long codiceAereo;
     private double prezzo;
 
     public Long getCodice() {
@@ -28,11 +44,12 @@ public class Volo {
         return aeroportoArrivo;
     }
 
-    public LocalDateTime getOrario() {
+    public Date getOrario() {
         return orario;
     }
+    
 
-    public int getCodiceAereo() {
+    public long getCodiceAereo() {
         return codiceAereo;
     }
 
@@ -48,11 +65,12 @@ public class Volo {
         this.aeroportoPartenza = aeroportoP;
     }
 
-    public void setOrario(LocalDateTime orario) {
+    public void setOrario(Date orario) {
         this.orario = orario;
     }
+    
 
-    public void setCodiceAereo(int codiceAereo) {
+    public void setCodiceAereo(long codiceAereo) {
         this.codiceAereo = codiceAereo;
     }
 
